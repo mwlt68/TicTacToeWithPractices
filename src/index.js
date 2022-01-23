37fb54aedb44efe546609e7d4d577a417320563b
mwlt68 <mwltgr@gmail.com>
2022-01-23 10:18:53 +0300

    Solution of practice-5

--- src/index.js
+++ src/index.js
@@ -2,23 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Square(props){
-  return (
-    <button className='square' onClick={props.onClick}>
-      {props.value}
-    </button>
-  );
-}
+  function Square(props){
+    return (
+      <button id={props.isActive ? 'winner-button':''}  className='square' onClick={props.onClick}>
+        {props.value}
+      </button>
+    );
+  }
 
-
-  
   class Board extends React.Component {
 
     renderSquare(i) {
+      let isActive =this.props.winnerIndexes
+      if(isActive){
+        const [a, b, c] = this.props.winnerIndexes;
+        isActive=a===i|| b===i|| c===i;
+      }
       return <Square
         value={this.props.squares[i]} 
         onClick={()=>this.props.onClick(i)}
         key={i}
+        isActive={isActive}
         />
     }
 
@@ -118,6 +122,7 @@ function Square(props){
       const history = this.state.history;
       const current = history[this.state.stepNumber];
       const winner = calculateWinner(current.squares);
+      
       const moves= history.map((step,move)=>{
         let moveVal=this.state.orderByAsc ? move : this.state.stepNumberBeforeClick-move;
         let desc='Go to game start';
@@ -142,7 +147,7 @@ function Square(props){
         status="Winner: "+winner;
       else
         status = 'Next player: '+(this.state.xIsNext ? 'X': 'O');
-
+      const winnerIndexes = getWinnerIndexes(current.squares);
       return (
         <div className="game">
           <div className="game-board">
@@ -150,6 +155,7 @@ function Square(props){
               squares={current.squares}
               onClick={(i)=>this.handleClick(i)}
               gameSize={this.props.gameSize}
+              winnerIndexes={winnerIndexes}
             />
           </div>
           <div className="game-info">
@@ -190,6 +196,26 @@ function Square(props){
       const [a, b, c] = lines[i];
       if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
         return squares[a];
+      }
+    }
+    return null;
+  }
+
+  function getWinnerIndexes(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return lines[i];
       }
     }
     return null;
